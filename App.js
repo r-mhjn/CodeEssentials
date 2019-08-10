9ecf6d2bd1c2cbd189896e8b63544eb33ba7a627
rachit mahajan <rachitmahajan9936@gmail.com>
2019-08-10 11:46:01 +0530

    new screen addded to navigator

--- App.js
+++ App.js
@@ -1,59 +1,53 @@
-import React from 'react';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
+import React from "react";
+import { createAppContainer, createStackNavigator } from "react-navigation";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 //TODO: Import screens here
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import ProfileScreenNotLoggedIn from './screens/ProfileScreenNotLoggedIn';
-import LoginScreen from './screens/LoginScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import EditScreen from './screens/EditScreen';
-import ResetPasswordScreen from './screens/ResetPasswordScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import AddQuestionScreen from './screens/AddQuestionScreen';
-// import DiscussScreen from './screens/DiscussScreen';
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ProfileScreenNotLoggedIn from "./screens/ProfileScreenNotLoggedIn";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import EditScreen from "./screens/EditScreen";
+import ResetPasswordScreen from "./screens/ResetPasswordScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import AddQuestionScreen from "./screens/AddQuestionScreen";
+import DisplayChallengeScreen from "./screens/DisplayChallengeScreen";
 
-const reducer  = () => [];
+const reducer = () => [];
 
 const store = createStore(reducer);
 
-
-const AppNavigatorStack  = createStackNavigator(
+const AppNavigatorStack = createStackNavigator(
   {
-    HomeScreen:{screen:HomeScreen},  
-    ProfileScreen:{screen:ProfileScreen},
-    ProfileScreenNotLogged:{screen:ProfileScreenNotLoggedIn},
-    LoginScreen:{screen:LoginScreen},
-    SignUpScreen:{screen:SignUpScreen},
-    ResetPasswordScreen:{screen:ResetPasswordScreen},
-    EditScreen:{screen:EditScreen},
-    SettingsScreen:{screen:SettingsScreen},
-    AddQuestionScreen:{screen:AddQuestionScreen},
-    // DiscussScreen:{screen:DiscussScreen},
-    },
-   {
-      headerMode:"none",
-      navigationOptions:{
-        headerVisible: false,
-      }
-   }     
-)
+    HomeScreen: { screen: HomeScreen },
+    ProfileScreen: { screen: ProfileScreen },
+    ProfileScreenNotLogged: { screen: ProfileScreenNotLoggedIn },
+    LoginScreen: { screen: LoginScreen },
+    SignUpScreen: { screen: SignUpScreen },
+    ResetPasswordScreen: { screen: ResetPasswordScreen },
+    EditScreen: { screen: EditScreen },
+    SettingsScreen: { screen: SettingsScreen },
+    AddQuestionScreen: { screen: AddQuestionScreen },
+    DisplayChallengeScreen: { screen: DisplayChallengeScreen }
+  },
+  {
+    headerMode: "none",
+    navigationOptions: {
+      headerVisible: false
+    }
+  }
+);
 
-const AppNavigator =  createAppContainer(AppNavigatorStack);
+const AppNavigator = createAppContainer(AppNavigatorStack);
 
-
-export default class App extends React.Component{
-
+export default class App extends React.Component {
   render() {
     return (
-       <Provider store={store}>
+      <Provider store={store}>
         <AppNavigator />
-       </Provider>
-    )
+      </Provider>
+    );
   }
-
-};
-
+}
