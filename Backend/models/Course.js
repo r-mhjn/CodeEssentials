88621c8cf2bffd6eed9afc152e29406439401c70
rachit mahajan <rachitmahajan9936@gmail.com>
2019-08-11 12:59:58 +0530

    Course model added

---
+++ Backend/models/Course.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema(
+  {
+    domain: [
+      {
+        domainName: {
+          type: String,
+          required: true
+        },
+        courses: [
+          {
+            name: {
+              type: String,
+              required: true
+            },
+            topics: [
+              {
+                topicName: {
+                  type: String,
+                  required: true
+                },
+                topicDescription: {
+                  type: String,
+                  required: true
+                },
+                addedOn: {
+                  type: Date,
+                  date: Date.now()
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = Course = mongoose.model("myCourse", CourseSchema);
