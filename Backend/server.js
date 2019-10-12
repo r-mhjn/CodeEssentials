01df2e9fa4b1ac7d77c6496cf0fd9efdefc06f5e
r-mhjn <rachitmahajan9936@gmail.com>
2019-10-13 02:52:54 +0530

    public folder added

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const passport = require("passport");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config(); //to read the dotenv file containing env variables
+const passport = require('passport');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config(); //to read the dotenv file containing env variables
 const port = process.env.PORT || 3030;
 const mongoUri = process.env.MONGOURI;
-const morgan = require("morgan");
+const morgan = require('morgan');
+
+app.use(express.static('./public'));
 
 // Middleware cors = cross origin policy (using this we can send req from one server to other)
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.use(morgan("short")); // // its a middleware which gives the details of the requests made to the server
+app.use(morgan('short')); // // its a middleware which gives the details of the requests made to the server
 
 // MiddleWare like bodyparser
 app.use(express.json());
@@ -32,21 +28,21 @@ app.use(express.json());
 app.use(passport.initialize());
 
 // Bring all routes
-const userRoute = require("./routes/user");
-const adminRoute = require("./routes/admin");
+const userRoute = require('./routes/user');
+const adminRoute = require('./routes/admin');
 
 //actual routes
-app.use("/user", userRoute);
-app.use("/admin", adminRoute);
+app.use('/user', userRoute);
+app.use('/admin', adminRoute);
 
 //Connect to database
 mongoose
-  .connect(mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("mongo connected sucessfully"))
-  .catch(err => console.log("error connecting database"));
+	.connect(mongoUri, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('mongo connected sucessfully'))
+	.catch(err => console.log('error connecting database'));
 
 app.listen(port, () => console.log(`server is running at port ${port}`));
