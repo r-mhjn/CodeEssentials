3d74736dd96c0dad534a1d7797008c7544cfc3f4
r-mhjn <rachitmahajan9936@gmail.com>
2019-10-13 02:54:08 +0530

    Schemas refactored

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,53 +1,76 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // TODO: gonna have to add the question id to the user ...to give out the courses by the user
 const UserSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    mycourses: [
-      {
-        courseId: {
-          type: String,
-          required: true
-        },
-        courseName: {
-          type: String,
-          required: true
-        },
-        adddedOn: {
-          type: Date,
-          date: Date.now()
-        },
-        completedOn: {
-          type: Date
-        }
-      }
-    ],
-    password: {
-      type: String,
-      required: true
-    },
-    phoneno: {
-      type: String,
-      required: true,
-      maxLength: 12
-    },
-    profilepic: {
-      type: String,
-      default:
-        "https://t4.ftcdn.net/jpg/01/11/89/61/500_F_111896141_yGxg122F73hHRuhJWtIq7i2VjP36vW6j.jpg"
-    }
-  },
-  {
-    timestamps: true
-  }
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		mycourses: [
+			{
+				courseId: {
+					type: String,
+					required: true,
+				},
+				courseName: {
+					type: String,
+					required: true,
+				},
+				adddedOn: {
+					type: Date,
+					date: Date.now(),
+				},
+				completedOn: {
+					type: Date,
+				},
+			},
+		],
+		bookmarkedCourses: [
+			{
+				courseId: {
+					type: String,
+					required: true,
+				},
+				bookmarkedOn: {
+					type: Date,
+					date: Date.now(),
+				},
+			},
+		],
+		solvedChallenges: [
+			{
+				challengeId: {
+					type: String,
+					required: true,
+				},
+				completedOn: {
+					type: Date,
+					date: Date.now(),
+				},
+			},
+		],
+		password: {
+			type: String,
+			required: true,
+		},
+		phoneno: {
+			type: String,
+			required: true,
+			maxLength: 12,
+		},
+		profilepic: {
+			type: String,
+			default: 'https://t4.ftcdn.net/jpg/01/11/89/61/500_F_111896141_yGxg122F73hHRuhJWtIq7i2VjP36vW6j.jpg',
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
-module.exports = User = mongoose.model("myUser", UserSchema);
+module.exports = User = mongoose.model('myUser', UserSchema);
